6b181f1f8c5b5db599e41974727c85dfe2b71f31
bjork-beep <bjhu0001@stud.kea.dk>
2025-11-02 17:52:12 +0000

    add_content

---
+++ script.js
@@ -0,0 +1,32 @@
+// menu
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
+
+/* FADE IN */
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+document.querySelectorAll(".fade-in").forEach((card) => {
+  observer.observe(card);
+});
